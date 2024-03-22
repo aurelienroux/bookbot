9a9d53a06fc1f44f77b1ac9def7a700d0757c883
aurelienroux <aurelien.roux@busbud.com>
2024-03-22 22:29:07 +0100

    Add main function to count words in a book

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+def main():
+  book_path = './books/frankenstein.txt'
+  book_text = get_book_text(book_path)
+  text_num_words = get_text_words_len(book_text)
+  print(text_num_words)
+    
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
+
+def get_text_words_len(text):
+  words_array = text.split()
+  return len(words_array)
+
+main()
